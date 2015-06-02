740f0ad43fbed35506d3332a02c6ba9c9ad9d184
Jeffrey David Johnson <jefdaj@gmail.com>
2015-06-01 17:53:34 -0700

    got build to work with haskellng and 10.6.3

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-with import <mypkgs>;
+with import <nixpkgs> {};
 let gitit =
 
 # Just use this middle part when embedding in nixpkgs:
@@ -7,14 +7,8 @@ let gitit =
 
 haskellngPackages.mkDerivation {
   pname   = "gitit";
-  version = "0.10.6.2";
-  # src = fetchgit {
-  #   url = "https://github.com/jefdaj/gitit.git";
-  #   rev = "3525826b0c88bfab31ebab1694189292d0ecadfa";
-  #   sha256 = "a9f15d7fb39c891c3fe5e7030660ec0ebf82feca933e423e54108639d680486b";
-  # };
+  version = "0.10.6.3";
   src = ./.;
-  isLibrary    = true;
   isExecutable = true;
   jailbreak    = true;
   # TODO where to add graphviz?
@@ -30,12 +24,10 @@ haskellngPackages.mkDerivation {
     # my additional dependencies
     canonical-filepath pandoc-citeproc
   ];
-  # meta = {
-    homepage    = "http://gitit.net";
-    description = "Wiki using happstack, git or darcs, and pandoc";
-    license     = "GPL"; # TODO get from stdenv?
-    # platforms = self.ghc.meta.platforms; # TODO add this back?
-  # };
+  homepage    = "http://gitit.net";
+  description = "Wiki using happstack, git or darcs, and pandoc";
+  license     = "GPL";
+  platforms   = haskellngPackages.ghc.meta.platforms;
 }
 
 ; in callPackage gitit {}
